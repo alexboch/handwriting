c17fb668e7cf19b5c22bb0fa48eda6e1341dcf8c
Alexander Bocharov <alexbocharov2@gmail.com>
2018-02-27 20:39:24 +0300

    Added words dictionary

--- prepare_data.py
+++ prepare_data.py
@@ -2,14 +2,44 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import os
-    
+
+
+def label_to_int(char_label):
+    return ord(char_label)-1072#Код буквы А=1072
+
 def load_lds(filename):
     raw_data=pd.read_json(filename,encoding='utf8')
     transposed_data=raw_data.T
     labeled_words=[]
-    
+    #Создать ключи словаря из слов всех текстов
+    words_dict=dict.fromkeys(np.hstack(transposed_data['TextWords'].ravel()))
     for text in transposed_data.iterrows():
         pl=text[1][['PointLists','Labels']]
+        #words_dict=dict.fromkeys(text_words)
+        grouped_words_data=[]
+        #flattened_points=[]
+        #flattened_labels=[]
+        for i in np.arange(len(pl.PointLists)):
+            points_list=pl.PointLists[i]
+            labels_list=pl.Labels[i]
+            for j in np.arange(len(points_list)):
+                point=points_list[j]
+                label=labels_list[j]
+                if label is not None:#Если не нулевая метка
+                    integer_label=label_to_int(label['Item1'])#Букву в число
+                
+            #flattened_points.append(points_list)
+            #flattened_labels.append(labels_list)
+            
+        
+            #for j in np.arange(len(points_list)):
+             #   flattened_points.append(poin)
+        
+        
+    #lists_pairs=zip(pl.PointLists,pl.Labels)#пары вида [([список точек],[список меток])]
+    #print(list(lists_pairs))
+    #Преобразовать пары в пары "Точка-метка"
+    
     #for text_id in transposed_data.Id:
     #    mask=transposed_data.Id==text_id#Получить текст
     
