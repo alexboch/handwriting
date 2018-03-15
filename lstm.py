d3e2a000d8c8a041e882d521424150df305df931
Alexander Bocharov <alexbocharov2@gmail.com>
2018-03-15 18:36:40 +0300

    111

--- lstm.py
+++ lstm.py
@@ -21,21 +21,23 @@ class LSTMDecoder:
         """
         Принимает список точек и возвращает последовательность меток 
         """
+        
         pass
     
     
     TINY  = 1e-6    # to avoid NaNs in logs
-    def train(self,words,num_epochs=1000):
+    def train(self,words,num_epochs=100):
         """
         words--Список слов, содержащих точки и метки
         """
-        init_op = tf.initialize_all_variables()
+        #init_op = tf.initialize_all_variables()
         session=tf.Session()
-        session.run(init_op)
+        session.run(tf.global_variables_initializer())
         num_batches=len(words)/self.batch_size
         num_words=len(words)
         for i in np.arange(num_epochs):
             print("Epoch number ",str(i))
+            np.random.shuffle(words)
             for j in np.arange(0,num_words,self.batch_size):
                 j1=j
                 j2=j1+self.batch_size
@@ -48,10 +50,11 @@ class LSTMDecoder:
                     batch_labels.append(w.labels_list)
                 inputs_arr=np.asarray(batch_inputs)
                 outputs_arr=np.asarray(batch_labels)
-                outputs_arr=np.expand_dims(outputs_arr,2)
+                outputs_arr=np.expand_dims(outputs_arr,2)#чтобы создать из числа-метки массив из одного числа
                 outputs_arr=outputs_arr.astype(float)
                 session.run(self.train_fn,feed_dict={self.inputs:inputs_arr,self.outputs:outputs_arr})
                 #session.run(self.train_fn,feed_dict={self.inputs:batch_inputs, self.outputs:batch_labels})
+        session.close()
         pass
         
     
@@ -64,11 +67,10 @@ class LSTMDecoder:
         self.outputs = tf.placeholder(tf.float32, (None, None, output_size))# (time, batch, out)
         cells=[]#список клеток
         dropout=tf.placeholder(tf.float32)
-        for _ in range(num_layers):#Создать клетки для слоев
-            cell=tf.contrib.rnn.BasicLSTMCell(num_units,state_is_tuple=True)
-            #cell=tf.contrib.rnn.DropoutWrapper(cell,output_keep_prob=1.0-dropout)
-            cells.append(cell)
-        self.cell=tf.contrib.rnn.MultiRNNCell(cells)#Создаем клетку из нескольких
+        cell=tf.contrib.rnn.LSTMCell(num_units,state_is_tuple=True)
+        self.cell=tf.contrib.rnn.MultiRNNCell([cell]*num_units,state_is_tuple=True)
+        self.W=tf.Variable(tf.truncated_normal([num_units,output_size],stddev=0.1))#Начальная матрица весов
+        b=tf.Variable(tf.constant(0.,shape=[output_size]))
         
         # Given inputs (time, batch, input_size) outputs a tuple
         #  - outputs: (time, batch, output_size)  [do not mistake with OUTPUT_SIZE]
@@ -78,20 +80,31 @@ class LSTMDecoder:
         #If cell.state_size is a tuple,
         #this should be a tuple of tensors having shapes [batch_size, s] for s in cell.state_size
         initial_state=self.cell.zero_state(batch_size,dtype=tf.float32)
+        #rnn_outputs--Тензор размерности [batch_size,max_time,cell.output_size],max_time--кол-во точек слова,cell.output_size=2(x,y координаты)
+        #rnn_state--последнее состояние
         rnn_outputs, rnn_states = tf.nn.dynamic_rnn(self.cell, self.inputs, initial_state=initial_state)
+        logits=tf.matmul(rnn_outputs,W)+b
+        
+        """
         #inputs shape:[max_time,batch_size,depth]
         # project output from rnn output size to OUTPUT_SIZE. Sometimes it is worth adding
         # an extra layer here.
+        #layers_fully_connected--Добавляет полносвязный слой
         final_projection = lambda x: layers.fully_connected(x, num_outputs=output_size, activation_fn=tf.nn.sigmoid)
         # apply projection to every timestep.
         predicted_outputs = map_fn(final_projection, rnn_outputs)
+        
+        
+        
         # compute elementwise cross entropy.
         error = -(self.outputs * tf.log(predicted_outputs + self.TINY) + (1.0 - self.outputs) * tf.log(1.0 - predicted_outputs + self.TINY))
         error = tf.reduce_mean(error)
+        
         # optimize
         self.train_fn = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(error)
-
+        #self.label_fn = 
         # assuming that absolute difference between output and correct answer is 0.5
         # or less we can round it to the correct output.
         self.accuracy = tf.reduce_mean(tf.cast(tf.abs(self.outputs - predicted_outputs) < 0.5, tf.float32))
+        """
         pass
