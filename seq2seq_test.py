40c25f9d8e43fae8cca06e66e946a673b9597131
Alexander Bocharov <alexbocharov2@gmail.com>
2018-05-09 16:32:45 +0300

    111

--- seq2seq_test.py
+++ seq2seq_test.py
@@ -3,14 +3,15 @@ import numpy as np
 
 sess=tf.InteractiveSession()
 #a=tf.nn.softmax(tf.constant([[[0,2]]],dtype=tf.float32))
-a=tf.constant([[[0,1],[0,2]]],dtype=tf.float32)
+a=tf.constant([[[0,200],[0,200],[200,0]]],dtype=tf.float32)
 sm=tf.nn.softmax(a)
 
 print( f"Unscaled logits:{a.eval()}")
 print(f"Softmax:{sm.eval()}")
-b=tf.constant([[1],[1]])
-w=tf.constant([[1.0],[1.0]])
+b=tf.constant([[0],[0],[0]])
+w=tf.constant([[1],[1],[2]],dtype=tf.float32)
 loss=tf.contrib.seq2seq.sequence_loss(a,b,weights=w)
+
 #tf.nn.softmax_cross_entropy_with_logits()
 le=loss.eval()
 print(le)
@@ -19,9 +20,13 @@ print(le)
 #b = tf.placeholder(tf.float32, shape = [None, 1])
 #a=tf.nn.softmax(tf.constant([[0,2]],dtype=tf.float32))
 #a=tf.constant([[[0,2]]],dtype=tf.float32)
+w=tf.constant([1.0,2.0])
 b=tf.constant([[0,1],[0,1]],dtype=tf.float32)
 sess.run(tf.global_variables_initializer())
 c = tf.nn.softmax_cross_entropy_with_logits(
     logits=a, labels=b
-).eval()
-print(c)
+)
+print(c.eval())
+c=c*w
+c=tf.reduce_mean(c)
+print(c.eval())
